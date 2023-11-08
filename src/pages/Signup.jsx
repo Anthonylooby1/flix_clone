2ed87ffc1d8a1efa78ba775ad6224d79b1191473
Anthony Looby <124841003+Anthonylooby1@users.noreply.github.com>
2023-11-08 18:10:49 -0500

    created user and stored into firebase

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,28 +1,66 @@
-import React from 'react'
+import { createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import BackgroundImage from '../components/BackgroundImage';
 import Header from '../components/Header';
+import { firebaseAuth } from '../utils/firebase-config';
+import { useNavigate } from 'react-router-dom'
 
 export default function Signup() {
+
+    const [showPassword, setShowPassword] = useState(false);
+    const [formValues, setFormValues] = useState({
+        email: "",
+        password: "",
+    });
+
+    const handleSignIn = async ()=> {
+        try {
+            const {email,password} = formValues;
+            await createUserWithEmailAndPassword(firebaseAuth, email, password)
+        } catch(err) {
+            console.log(err)
+        }
+    };
+
+    const navigate = useNavigate()
+
+    onAuthStateChanged(firebaseAuth,(currentUser) => {
+        if(currentUser) navigate("/");
+    });
+
   return (
-    <Container>
+    <Container showPassword={showPassword}>
         <BackgroundImage />
         <div className="content">
-        <Header />
+        <Header login />
         <div className="body flex column a-center j-center">
             <div className="text flex column">
                 <h1>Unlimited Movies, TV Shows, and more</h1>
                 <h4>Watch anywhere. Cancel Anytime</h4>
-                <h6>Reafy to watch? Enter your email to create or restart membership</h6>
+                <h6>Ready to watch? Enter your email to create or restart membership</h6>
             </div>
             <div className="form">
-                <input type='email' placeholder='Email' name='email'></input>
-                <input type='password' placeholder='Password' name='password'></input>
-                <button>Get Started</button>
-            <div>
-                <button>Login</button>
+                <input type='email' placeholder='Email' name='email' 
+                    value={formValues.email} 
+                    onChange={(e)=>setFormValues({
+                        ...formValues,
+                        [e.target.name]:e.target.value,
+                    })
+                } />
+                {showPassword && (<input type='password' placeholder='Password' name='password' 
+                value={formValues.password} 
+                onChange={(e)=>setFormValues({
+                        ...formValues,
+                        [e.target.name]:e.target.value,
+                    })
+                    }/>
+                )}
+                {!showPassword && (
+                    <button onClick={()=>setShowPassword(true)}>Get Started</button>
+                )}
             </div>
-            </div>
+            <button onClick={handleSignIn}>Sign Up</button>
         </div>
         </div>
     </Container>
@@ -40,5 +78,50 @@ const Container = styled.div`
         width: 100vw;
         display: grid;
         grid-template-rows: 15vh 85vh;
+        .body {
+            gap: 1rem;
+            .text {
+                gap: 1rem;
+                text-align: center;
+                font-size: 2rem;
+                h1 {
+                    padding: 0 25rem;
+                }
+            }
+            .form {
+                display: grid;
+                grid-template-columns:${({showPassword})=>showPassword ? "1fr 1fr": "2fr 1fr"}
+                width: 60%;
+                input {
+                    color: black;
+                    border: none;
+                    padding: 1.5rem;
+                    font-size: 1.2rem;
+                    border: 1px solid black;
+                    &:focus {
+                        outline: none;
+                    }
+                }
+                button {
+                    padding: 0.5rem 1rem;
+                    background-color: #e50914;
+                    border: none;
+                    cursor: pointer;
+                    color: white;
+                    font-weight: bolder;
+                    font-size: 1.05rem;
+                }
+            }
+            button {
+                padding: 0.5rem 1rem;
+                background-color: #e50914;
+                border: none;
+                cursor: pointer;
+                color: white;
+                border-radius: 0.2rem;
+                font-weight: bolder;
+                font-size: 1.05rem;
+            }
+        }
     }
 `;
--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -1,6 +1,6 @@
-// Import the functions you need from the SDKs you need
+
+import { getAuth } from 'firebase/auth'
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBASzdvP0CdEr1ZJxTx8YTRGZODfs5zOpI",
