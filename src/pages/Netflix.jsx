75f56dce95d62f1a220ca70ae8602a1120a655b2
Anthony Looby <124841003+Anthonylooby1@users.noreply.github.com>
2023-11-11 17:43:29 -0500

    added login and logout

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
+import NavBar from "../components/NavBar"
 
 export default function Netflix() {
+
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () =>  window.onscroll = null
+  }
   return (
-    <div>Netflix</div>
+    <div>
+        <NavBar isScrolled={isScrolled} />
+    </div>
   )
 }
